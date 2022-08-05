983cec2cddd19a222cf09bab9d750bf9f15a6259
Rodgers Chayuga <rodgers.chayuga@baanx.com>
2022-08-05 13:05:34 +0300

    fix: clear defolt search

--- src/features/search/searchSlice.ts
+++ src/features/search/searchSlice.ts
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 import { IProfileStateType, IReposStateType } from '../../app/types';
 
 export const userInitial = {
-  user: 'chayuga',
+  user: '',
 } as IReposStateType | IProfileStateType;
 
 // Creating slice for Search
